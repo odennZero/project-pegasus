bec17c4894c9bf39cf5b97e15bc0ac0905091395
shaunforde <shaun.forde@gmail.com>
2014-01-08 11:58:46 +0000

    Added IP detection message for your changes. Lines starting

--- src/android/MacAddressPlugin.java
+++ src/android/MacAddressPlugin.java
@@ -73,8 +73,21 @@ public class MacAddressPlugin extends CordovaPlugin {
             macAddress = "No Mac Address Found!";
         }
 
-        macAddress = "test override";
+        Integer rawIP = wm.getConnectionInfo().getIpAddress();
+
+        String ip = intToIp(rawIP);
+        
+        macAddress = ip;
 
         return macAddress;
     }
+
+      public String intToIp(int i) {
+
+           return ( i & 0xFF)  + "." +
+                   ((i >> 8 ) & 0xFF) + "." +
+                   ((i >> 16 ) & 0xFF) + "." +
+                   ((i >> 24 ) & 0xFF ) ;
+           
+        }
 }

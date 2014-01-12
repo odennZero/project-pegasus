38aa3c8baa5126ec227495fd9498175dec04c5ce
shaunforde <shaun.forde@gmail.com>
2014-01-12 22:04:37 +0000

    Add radial scan loop in checkHosts, bare without scanning - just test logs

--- src/android/MacAddressPlugin.java
+++ src/android/MacAddressPlugin.java
@@ -112,44 +112,116 @@ public class MacAddressPlugin extends CordovaPlugin {
         //test loop:
             
       public void checkHosts(String subnet){
-           int timeout=200;
-           
-           System.out.println("Starting ip loop for " + subnet);
-           
-           WifiManager wm = (WifiManager) this.cordova.getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-           
-           Integer rawIP = wm.getConnectionInfo().getIpAddress();
+        
+         int timeout=200;
+         
+         System.out.println("Starting ip loop for " + subnet);
+         
+         WifiManager wm = (WifiManager) this.cordova.getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+         
+         Integer rawIP = wm.getConnectionInfo().getIpAddress();
            String ip = intToIp(rawIP);
            
-           //get node subnet address as int (convert back to string during scan)
+         //get node subnet address as int (convert back to string during scan)
            
            int ipCenter = Integer.parseInt(ip.substring(10));
            
            System.out.println("IP Center is: " + (ipCenter));
            
+           //RADIUS SCAN Loop, starting with the host devices Ip as the center:
            
+         //set the direction to up by default
+           String direction = null;
+           if(ipCenter<254){direction = "up";}else{direction = "down";}
            
-           for (int i=1;i<254;i++){
+           //use negative int conversion to change direction (so it will go one up then one down, two up then two down etc etc):
+           
+           int highest = 2; 
+           int lowest = 2;
+           int actionCount = 0;
+           //loop while high is less than 254 and low is greater than 1:
+           System.out.println(direction);
+           
+           for(int i=1; highest <254 || lowest >1; ){
+             
+             int iptoScan = 0;
+             
+             //set the direction of i:
+             if(direction == "up"){iptoScan = ipCenter + i;}else{iptoScan = ipCenter + (i*-1);}
+             
+             
+             //do the thing:
+             System.out.println(i);
+             System.out.println("Radial Scan: " + subnet + "." + iptoScan);
+             
+             
+             //switch up/down AND set the highest and lowest counters - only if there is room to go up or down:
+             
+             if(lowest<1 || highest>254){
+            
+               if(lowest<1){
+                 direction = "up";
+                 highest = ipCenter + i;
+               //increment action count by two to skip repeated process (no longer need to use i twice as we are only going in one direction!)
+                     actionCount = actionCount +2;
+               }
                
-               String host=subnet + "." + i;
-               System.out.println("Trying: " + host );
-               try {
-                if (InetAddress.getByName(host).isReachable(timeout)){
-                       System.out.println(host + " is reachable and is called: " + InetAddress.getByName(host).getHostName());
-                       
-                       
-                   }
-            } catch (UnknownHostException e) {
-                System.out.println("Unknown Host Error");
-                // TODO Auto-generated catch block
-                //e.printStackTrace();
-            } catch (IOException e) {
-                System.out.println("IO Exception Error");
-                // TODO Auto-generated catch block
-                //e.printStackTrace();
-            }
+               if(highest>254){
+                 
+                 direction ="down";
+                 lowest = ipCenter + (i*-1);
+               //increment action count by two to skip repeated process
+                     actionCount = actionCount +2;
+               }
+               
+             }else{
+               
+               //switch up/down AND set the highest and lowest counters
+               if(direction == "up"){direction = "down";highest = ipCenter + i;}else{direction ="up";lowest=ipCenter + (i*-1);}   
+             //count actions per digit increment (to ensure up +x and down -x are both done before x is incremented)
+                 actionCount ++;
+             }
+            
+                   
+             //increment if this has been done twice:
+             if(actionCount> 1){
+              i++;
+              //reset the action count:
+              actionCount = 0;
+             }
+             
+             
            }
-        }
+           
+           
+           
+           
+//         for (int i=1;i<254;i++){
+//           
+//             String host=subnet + "." + i;
+//             System.out.println("Trying: " + host );
+//             try {
+//        if (InetAddress.getByName(host).isReachable(timeout)){
+//               System.out.println(host + " is reachable and is called: " + InetAddress.getByName(host).getHostName());
+//               
+//               
+//           }
+//      } catch (UnknownHostException e) {
+//        System.out.println("Unknown Host Error");
+//        // TODO Auto-generated catch block
+//        //e.printStackTrace();
+//      } catch (IOException e) {
+//        System.out.println("IO Exception Error");
+//        // TODO Auto-generated catch block
+//        //e.printStackTrace();
+//      }
+//         }
+         
+         
+         
+         
+         
+      }
       
       
       
@@ -158,12 +230,12 @@ public class MacAddressPlugin extends CordovaPlugin {
           public ArrayList<String> getConnectedIps(){
         
         
-              //get the IP Address:
-              
-              String fullIP = getMacAddress();
-              
-              String baseIP = fullIP.substring(0,9);
-                      
+            //get the IP Address:
+            
+            String fullIP = getMacAddress();
+            
+            String baseIP = fullIP.substring(0,9);
+                    
              //function to get an array of all connected IP's on the subnet:
                      
               ArrayList<String> hosts = new ArrayList<String>();
